c1c4ff732134e5a1467b4129e7d20e82b12185e5
Guilherme Reinol <guibbv2011@hotmail.com>
2025-10-02 17:25:09 -0300

    Post photos api
    Already working post photo api.
    
    this api handles a file 'photo' and extract from contents the original
    filename.

--- server/essenziale_storage/lib/essenziale_storage.dart
+++ server/essenziale_storage/lib/essenziale_storage.dart
@@ -24,6 +24,40 @@ void main() async {
     return Response.ok(videos);
   });
 
+  router.post('/api/upload/photos', (Request request) async {
+    if (request.formData() case var formData?) {
+      List<int>? fileBytes;
+      String? nameFile;
+
+      await for (final field in formData.formData) {
+        if (field.name == 'file') {
+          RegExp r = RegExp(r'filename="([^"]+)"');
+          Match? match = r.firstMatch(
+            field.part.headers['content-disposition']!,
+          );
+
+          if (match != null && match.group(1) != null) {
+            nameFile = match.group(1)!;
+          }
+
+          fileBytes = await field.part.readBytes();
+          break;
+        }
+      }
+
+      if (fileBytes == null) {
+        return Response(400, body: 'No file uploaded');
+      }
+
+      final File file = File('assets/${nameFile!.replaceFirst('/', '.')}');
+
+      await file.parent.create(recursive: true);
+      await file.writeAsBytes(fileBytes);
+
+      return Response.ok('Received file: $fileBytes');
+    }
+  });
+
   router.post('/api/upload/videos', (Request request) async {
     final body = await request.readAsString();
     return Response.ok('Received: $body');
@@ -35,3 +69,4 @@ void main() async {
 
   final server = await serve(handler, 'localhost', 8080);
 }
+
