da1e5d7ddea80edff282a3c9a1347a109b2941aa
Guilherme Reinol <guibbv2011@hotmail.com>
2025-10-07 13:11:38 -0300

    add new routers to map

--- server/essenziale_storage/lib/essenziale_storage.dart
+++ server/essenziale_storage/lib/essenziale_storage.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
 
+import 'package:essenziale_storage/routers/delete_file.dart';
+import 'package:essenziale_storage/routers/delete_index.dart';
 import 'package:essenziale_storage/routers/get_file.dart';
-import 'package:essenziale_storage/routers/get_filenames.dart';
 import 'package:essenziale_storage/routers/post_files.dart';
+import 'package:essenziale_storage/routers/read_filenames.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_dynamic_forwarder/shelf_dynamic_forwarder.dart';
 
 final Map<String, Handler> dynamicRoutes = {
+  'deleteIndex': deleteIndexRequest,
+  'deleteFile': deleteFileRequest,
   'filenames': filenamesRequest,
   'media': filesUpload,
   '.*': fileRequest,
