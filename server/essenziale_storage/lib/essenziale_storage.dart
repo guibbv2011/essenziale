50bbe50940b935892275f59664985a8aa33b272b
Guilherme Reinol <guibbv2011@hotmail.com>
2025-10-08 16:31:48 -0300

    Config and initialize GCS client

--- server/essenziale_storage/lib/essenziale_storage.dart
+++ server/essenziale_storage/lib/essenziale_storage.dart
@@ -8,13 +8,28 @@ import 'package:essenziale_storage/routers/read_filenames.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_dynamic_forwarder/shelf_dynamic_forwarder.dart';
+import 'package:googleapis/storage/v1.dart' as storage;
+import 'package:googleapis_auth/auth_io.dart' as auth;
+
+late final auth.AuthClient authenticatedClient;
+late final storage.StorageApi gcsClient;
+
+const String bucketName = 'essenziale-bucket-server1';
+const List<String> _scopes = [storage.StorageApi.cloudPlatformScope];
+
+Future<void> initializeGcsClient(String projectId, String bucketName) async {
+  authenticatedClient = await auth.clientViaApplicationDefaultCredentials(
+    scopes: _scopes,
+  );
+  gcsClient = storage.StorageApi(authenticatedClient);
+}
 
 final Map<String, Handler> dynamicRoutes = {
-  'deleteIndex': deleteIndexRequest,
-  'deleteFile': deleteFileRequest,
-  'filenames': filenamesRequest,
-  'media': filesUpload,
-  '.*': fileRequest,
+  'deleteIndex': deleteIndexRequest(gcsClient, bucketName),
+  'deleteFile': deleteFileRequest(gcsClient, bucketName),
+  'filenames': filenamesRequest(gcsClient, bucketName),
+  'media': filesUpload(gcsClient, bucketName),
+  '.*': fileRequest(gcsClient, bucketName),
 };
 
 Handler get router {
@@ -27,17 +42,17 @@ Handler get router {
 }
 
 void main() async {
-  bool dir = await Directory('./assets').exists();
-  if (!dir) {
-    await Directory('./assets').create();
-  }
+  initializeGcsClient('essenziale-server', bucketName);
 
-  // final client = await auth.clientViaMetadataServer();
-  // final bucket = StorageApi(client);
+  bool dir = await Directory('./tmp').exists();
+  if (!dir) {
+    await Directory('./tmp').create();
+  }
 
   final handler = const Pipeline()
       .addMiddleware(logRequests())
       .addHandler(router);
 
-  await serve(handler, 'localhost', 8080);
+  await serve(handler, '0.0.0.0', 8080);
+  print('Server listening on port: 8080');
 }
