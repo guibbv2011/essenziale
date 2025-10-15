a9cb1995ba2dcc18598f0c4814263f3294dba506
Guilherme Reinol <guibbv2011@hotmail.com>
2025-10-15 12:41:13 -0300

    remove auth via key.json

--- server/essenziale_storage/lib/essenziale_storage.dart
+++ server/essenziale_storage/lib/essenziale_storage.dart
@@ -1,38 +1,27 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:essenziale_storage/routers/delete_file.dart';
 import 'package:essenziale_storage/routers/delete_index.dart';
 import 'package:essenziale_storage/routers/get_file.dart';
 import 'package:essenziale_storage/routers/post_files.dart';
 import 'package:essenziale_storage/routers/read_filenames.dart';
+import 'package:googleapis/storage/v1.dart';
 import 'package:googleapis_auth/auth_io.dart';
+import 'package:http/http.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_dynamic_forwarder/shelf_dynamic_forwarder.dart';
-import 'package:googleapis/storage/v1.dart';
-import 'package:googleapis_auth/googleapis_auth.dart' as auth;
-import 'package:http/http.dart';
 
 late StorageApi gcsClient;
 
 const String projectId = 'essenziale-server';
 const String bucketName = 'essenziale-bucket-server1';
-final _scopes = [StorageApi.devstorageReadWriteScope];
+final _scopes = [StorageApi.cloudPlatformScope];
 
-Future<auth.AuthClient> getStorageClient() async {
+Future<AutoRefreshingAuthClient> getStorageClient() async {
   try {
-    final readFile = await File('/key.json').readAsString();
-    final jsonfile = jsonDecode(readFile) as Map<String, dynamic>;
-
-    final accountCredentials = auth.ServiceAccountCredentials.fromJson(
-      jsonfile,
-    );
     final Client client = Client();
 
-    auth.AuthClient authClient = await clientViaServiceAccount(
-      accountCredentials,
-      _scopes,
+    final authClient = await clientViaApplicationDefaultCredentials(
+      scopes: _scopes,
     );
 
     client.close();
@@ -63,11 +52,6 @@ Handler get router {
 void main() async {
   final authClient = await getStorageClient();
   gcsClient = StorageApi(authClient);
-
-  bool dir = await Directory('/tmp').exists();
-  if (!dir) {
-    await Directory('/tmp').create();
-  }
 
   final handler = const Pipeline()
       .addMiddleware(logRequests())
